270cb417b5842d8538177d0666b0f89d7f1219b7
Leo <leonhardgentner@gmx.de>
2018-06-12 11:17:13 +0200

    k

---
+++ Jetski.java
@@ -0,0 +1,9 @@
+import ea.*;
+public class Jetski extends SPIELER
+{
+    public Jetski(MODEL newm)
+    {
+        super(newm);
+        textur = new Figur(PosX,PosY,"Recources/jetski.eaf");
+    }
+}
