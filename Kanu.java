5d8f8802ae9940aa0ed02cb892ada1f1de9662cf
Michi <@>
2018-06-12 11:00:48 +0200

    a

---
+++ Kanu.java
@@ -0,0 +1,11 @@
+import ea.*;
+public class Kanu extends SPIELER
+{
+    public Kanu(MODEL newm)
+    {
+        super(newm);
+        textur = new Figur(PosX,PosY,"Recources/Kanu.eaf");
+    }
+}
+
+        
