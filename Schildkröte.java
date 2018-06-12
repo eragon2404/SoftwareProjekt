d3227e19fb567196be27a37624282bf6a6404d5c
Pascal <@>
2018-06-12 10:49:13 +0200

    schildkröte

---
+++ Schildkröte.java
@@ -0,0 +1,9 @@
+import ea.*;
+public class Schildkröte extends SPIELER
+{
+    public Schildkröte(MODEL newm)
+    {
+        super(newm);
+        textur = new Figur(PosX,PosY,"Recources/schildkröte.eaf");
+    }
+}
